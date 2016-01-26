d58002474660853959a4e8b89e73e49ee04228a2
Thomas Ruble <sketchbang@gmail.com>
2016-01-26 08:44:55 -0600

    now it is a nice skeleton template

--- manage.py
+++ manage.py
@@ -3,12 +3,14 @@ from flask.ext.script import Manager, Shell
 
 from penelope.models import Track
 from penelope.api import create_app
+from penelope.database import init_db, db_session
 
 app = create_app()
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app, Track=Track)
+    return dict(app=app, Track=Track, sess=db_session, init_db=init_db)
+
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
