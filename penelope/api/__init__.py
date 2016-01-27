b403ac7359ee4fc214c5e4f86c4ce1d9f85923fa
Thomas Ruble <sketchbang@gmail.com>
2016-01-27 09:21:33 -0600

    add api track blueprint

--- penelope/api/__init__.py
+++ penelope/api/__init__.py
@@ -17,3 +17,21 @@ def create_app(settings_override=None, register_security_blueprint=False):
     app.json_encoder = JSONEncoder
 
     return app
+
+
+def route(bp, *args, **kwargs):
+    kwargs.setdefault('strict_slashes', False)
+
+    def decorator(f):
+        @bp.route(*args, **kwargs)
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            sc = 200
+            rv = f(*args, **kwargs)
+            if isinstance(rv, tuple):
+                sc = rv[1]
+                rv = rv[0]
+            return jsonify(dict(data=rv)), sc
+        return f
+
+    return decorator
