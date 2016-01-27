9478d9b35b4be1e22db7435d3f12a7dc9bc969b4
Thomas Ruble <sketchbang@gmail.com>
2016-01-27 07:38:46 -0600

    add migration command

--- manage.py
+++ manage.py
@@ -3,13 +3,14 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager, Shell
 
 from penelope.api import create_app
+from penelope.core import db                        
 from penelope.models import Track
 from penelope.tracks import TrackHandler
 from penelope.database import init_db, db_session
 
 app = create_app()
 manager = Manager(app)
-
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, Track=Track, sess=db_session, init_db=init_db,
