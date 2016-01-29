5ba60814fd6552a9c8385ad0a15b0234ba1897d9
Thomas Ruble <sketchbang@gmail.com>
2016-01-29 20:38:00 +0000

    something strange happened

--- penelope/frontend/static/js/directives.js
+++ penelope/frontend/static/js/directives.js
@@ -2,14 +2,25 @@
 
 var penelopeDirectives = angular.module('penelopeDirectives', []);
 
-penelopeDirectives.directive('masterRow', function() {
+penelopeDirectives.directive('masterRow', function () {
     return {
         templateUrl: '/static/partials/templates/master_row.html',
         restrict: 'A',
+        link: function ($scope, element, attrs) {
+            element.bind('click', function () {
+                element.html('You clicked me!');
+            });
+            element.bind('mouseenter', function () {
+                element.css('background-color', 'yellow');
+            });
+            element.bind('mouseleave', function () {
+                element.css('background-color', 'white');
+            });
+        }
     };
 });
 
-penelopeDirectives.directive('masterHead', function() {
+penelopeDirectives.directive('masterHead', function () {
     return {
         templateUrl: '/static/partials/templates/master_head.html',
         restrict: 'A'
