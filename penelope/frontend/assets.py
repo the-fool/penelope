903be67405d7c67ae5b11ad6d1188f654a818120
Thomas Ruble <sketchbang@gmail.com>
2016-01-29 11:31:49 -0600

    fixed header scrollable table

--- penelope/frontend/assets.py
+++ penelope/frontend/assets.py
@@ -1,23 +1,28 @@
+# -*- coding: utf-8 -*-
 from flask_assets import Environment, Bundle
 
+# : application css bundle
 
-#: application css bundle
-css_penelope = Bundle("less/penelope.less",
-                       filters="less", output="css/penelope.css",
-                       debug=False)
+css_penelope = Bundle('css/penelope.css')
 
-#: consolidated css bundle
-css_all = Bundle("bower_components/bootstrap/dist/css/bootstrap.min.css")
+# : consolidated css bundle
 
-#: vendor js bundle
-js_vendor = Bundle("bower_components/jquery/dist/jquery.js",
-                    "bower_components/bootstrap/dist/js/bootstrap.js",
-                   "bower_components/angular/angular.js",
-                   "bower_components/angular-route/angular-route.js",
-                   "bower_components/angular-resource/angular-resource.js")
+css_all = Bundle('bower_components/bootstrap/dist/css/bootstrap.min.css'
+                 )
 
-#: application js bundle
-js_main = Bundle("js/*.js")
+# : vendor js bundle
+
+js_vendor = Bundle('bower_components/jquery/dist/jquery.js',
+                   'bower_components/bootstrap/dist/js/bootstrap.js',
+                   'bower_components/angular/angular.js',
+                   'bower_components/angu-fixed-header-table/angu-fixed-header-table.js',
+                   'bower_components/angular-route/angular-route.js',
+                   'bower_components/angular-resource/angular-resource.js'
+                   )
+
+# : application js bundle
+
+js_main = Bundle('js/*.js')
 
 
 def init_app(app):
@@ -25,6 +30,9 @@ def init_app(app):
     webassets.register('css_all', css_all)
     webassets.register('js_vendor', js_vendor)
     webassets.register('js_main', js_main)
-    webassets.manifest = 'cache' if not app.debug else False
+    webassets.register('css_penelope', css_penelope)
+    webassets.manifest = ('cache' if not app.debug else False)
     webassets.cache = not app.debug
     webassets.debug = app.debug
+
+
--- penelope/frontend/static/js/penelope.js
+++ penelope/frontend/static/js/penelope.js
@@ -2,10 +2,12 @@
 
 var penelopeApp = angular.module('penelopeApp', [
     'ngRoute',
+    'anguFixedHeaderTable',
     'penelopeControllers',
     'penelopeDirectives',
     'penelopeServices',
     'penelopeFilters'
+    
 ]);
 
 penelopeApp.config(['$routeProvider', function ($rp) {
