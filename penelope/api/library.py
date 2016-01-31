e637eefcfc49ca470be161216c85f0bfeae88d69
the-fool <sketchbang@gmail.com>
2016-01-31 09:10:53 -0600

    css and backend fix, together in one commit

--- penelope/api/library.py
+++ penelope/api/library.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, send_file, current_app
+import codecs
+
+from flask import Blueprint, current_app
+from . import route
 
 bp = Blueprint('library', __name__, url_prefix='/library')
 
 @bp.route('/')
 def get_library():
-    return send_file(current_app.config['LIBRARY_CACHE'])
+    with codecs.open(current_app.config['LIBRARY_CACHE'], 'r', 'utf-8') as library:
+        data = library.read()
+    return data, 200, {'Content-Type': 'application/json; charset=utf-8'}
